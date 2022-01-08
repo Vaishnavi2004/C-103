bbba68778682dbd815278ba00dbb5df344ee3562
Vaishnavi2004 <78804893+Vaishnavi2004@users.noreply.github.com>
2022-01-08 18:03:01 +0530

    Add files via upload

---
+++ scatter.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import plotly_express as px
+
+df = pd.read_csv("data.csv")
+fig = px.scatter(df,x="Population",y="Per capita",color="Country",size="Percentage",size_max=60)
+
+fig.show()
